0c6ec4e30bc33c13d30128eb825bb13457b7468e
Prasad Kute <prasadkute0708@gmail.com>
2024-09-29 16:28:04 +0530

    auth context solved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {  Route, createBrowserRouter, createRoutesFromElements,  RouterProvider } from "react-router-dom";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import Login from "./components/authentications/Login";
 import Register from "./components/authentications/Register";
 import Homepage from "./components/homepages/Homepage";
@@ -15,19 +15,20 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
 
-      <Route index element={<Homepage />}  />
-      <Route path="about" element={<About />}  />
+      <Route index element={<Homepage />} />
+      <Route path="about" element={<About />} />
       <Route path="login" element={<Login />} />
       <Route path="register" element={<Register />} />
 
-      <Route path="/developer" element={<DeveloperLayout />}>
-      
-      <Route index element={<ProtectedRoute child={<Home />} />} />
+      <Route path="developer" element={<DeveloperLayout />}>
+
+        <Route index element={<ProtectedRoute child={<Home />} />} />
+        <Route path="documentation" element={<ProtectedRoute child={<div>document</div>} />} />
       </Route>
       {/* <Route path="resetPassword" element={<ResetPassword />} /> */}
 
 
-  {/* add routes */}
+      {/* add routes */}
 
 
       {/* <Route path="*" element={<NotFound />} /> */}
@@ -37,7 +38,7 @@ const router = createBrowserRouter(
 const App = () => {
   return (
     <AuthProvider>
-     <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </AuthProvider>
   );
 };
